b3e9bc70c7f94fdf0c47a925ad3eb14b4765525a
nieroda <eathotlead@gmail.com>
2018-07-07 18:21:07 -0400

    quick mod

--- backEnd/models/index.js
+++ backEnd/models/index.js
@@ -7,8 +7,5 @@ mongoose.connect(`mongodb://nkamm:boson@ds014658.mlab.com:14658/testlab`, () => 
 
 mongoose.Promise = Promise;
 
-//module.exports = mongoose;
-//module.exports.dummyModel  = require('./dummyModel')
-//module.exports.commentModel = require('./commentModel')
 module.exports.tradeModel = require('./tradeModel')
 module.exports.userModel = require('./userModel')
--- backEnd/tests/redis.test.js
+++ backEnd/tests/redis.test.js
@@ -1,12 +1,12 @@
 const redis = require('redis')
 
 let client
-const nathan = "76561198197292179"
+const s64_id = "76561198197292179"
 const dummyData = [{"item_id":80,"effect":null,"itemData":"v"},{"item_id":86,"effect":null,"itemData":"v"},{"item_id":251,"effect":null,"itemData":""},{"item_id":9999,"effect":6,"itemData":"u"},{"item_id":505,"effect":6,"itemData":"u"},{"item_id":368,"effect":6,"itemData":"su"}]
 const mongoID = "_id507f191e810c19729de860ea"
 
 const fakeTrade = {
-  user: nathan,
+  user: s64_id,
   mongoID,
   dummyData,
   toHave: dummyData
@@ -14,7 +14,7 @@ const fakeTrade = {
 
 function insertRedis() {
   return new Promise((resolve, reject) => {
-    client.lpush(`trade:${nathan}`, JSON.stringify(fakeTrade), (err, result) => {
+    client.lpush(`trade:${s64_id}`, JSON.stringify(fakeTrade), (err, result) => {
       return err ? reject(false) : resolve(true)
     })
   })
@@ -22,7 +22,7 @@ function insertRedis() {
 
 function trimTrade() {
   return new Promise((resolve, reject) => {
-    client.ltrim(`trade:${nathan}`, '0', '9', (err, resp) => {
+    client.ltrim(`trade:${s64_id}`, '0', '9', (err, resp) => {
       return err ? reject(false) : resolve(true)
     })
   })
@@ -30,7 +30,7 @@ function trimTrade() {
 
 function queryRedis() {
   return new Promise((resolve, reject) => {
-    client.lrange(`trade:${nathan}`, '0', '-1', (err, res) => {
+    client.lrange(`trade:${s64_id}`, '0', '-1', (err, res) => {
       return err ? reject(100) : resolve(res.length)
     })
   })
@@ -39,18 +39,17 @@ function queryRedis() {
 
 
 describe('We can create 10 trades in redis max', () => {
-
   beforeAll(async () => {
     client = redis.createClient()
   })
 
-  for (let i = 0; i < 20; i++) {
-    test(`Create trade ${i}`, async () => {
+  test(`Creating trade, testing values`, async () => {
+    for (let i = 0; i < 200; i++) {
       expect(await insertRedis()).toBeTruthy()
       expect(await trimTrade()).toBeTruthy()
       expect(await queryRedis()).toBeLessThan(11)
-    });
-  }
+    }
+  })
 
   afterAll(async () => {
     await client.quit()
